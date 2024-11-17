97f8552a2cf4a861e4392eb8002b9141de18f1a8
Saye Orle <105493042+Saye0@users.noreply.github.com>
2024-11-17 11:51:26 +0400

    Update db.js

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const connect = await mongoose.connect(process.env.MONGO_URI)
-        console.log("savurdu yarrani")
+        console.log("Connected to MongoDB")
 
     } catch (error) {
         console.log(error)
